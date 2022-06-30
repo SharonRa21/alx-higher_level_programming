950aa2c872b3f7968974cc3202c228701aaa57de
root <shazzy.rank@gmail.com>
2022-06-30 15:02:07 -0700

    modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
-def argv(*args):
-    a = ""
-    if len(args) <= 1:
-        a = "{} argument.".format(len(args))
+#!/usr/bin/python3
+def main():
+    import sys
+    n = len(sys.argv) - 1
+    if n > 1:
+         print(f"{n} arguments:")
+    elif n == 0:
+        print(f"{n} arguments.")
+        return
     else:
-        a = "{} arguments:".format(len(args))
+        print(f"{n} argument:")
+    ind = 1
+    for arg in range(n):
+        print(f"{ind}: {sys.argv[ind]}")
+        ind += 1
 
-    while True:
-        print(a)
-        for i, n in enumerate(args):
-            print("{}: {}".format(i+1, n))
-        break
-
-if __name__ == '__main__':
-    argv("Sharon", "Sam")
+if __name__ == "__main__":
+     main()
